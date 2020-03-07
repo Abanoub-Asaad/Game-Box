333732125a748d6aa9b32a78b06124f8c891e355
Abanoub-Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2020-03-07 12:10:34 +0200

    Test_Tetris

---
+++ GameBox_Tetris/src/gamebox_tetris/GameBox_Tetris.java
@@ -0,0 +1,19 @@
+
+package gamebox_tetris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class GameBox_Tetris extends Application 
+{
+    
+    @Override
+    public void start(Stage Tetris_Stage) 
+    {
+        
+        UI ui_obj = new UI (Tetris_Stage);      
+        
+    }
+
+}
---
+++ GameBox_Tetris/src/gamebox_tetris/UI.java
@@ -0,0 +1,35 @@
+
+package gamebox_tetris;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+
+public class UI
+{
+    private static Group root_tetris = new Group() ;
+    private Image tetris_back_img ;
+    private ImageView tetris_back_iv ;
+    
+    public UI(Stage Tetris_Stage)
+    {
+        
+        
+        String name ="file:resources/c.png" ;  
+        tetris_back_img = new Image(name,1400,800,false,false) ;
+        tetris_back_iv  = new ImageView(tetris_back_img);
+        root_tetris.getChildren().addAll (tetris_back_iv );
+        
+        Scene scene = new Scene(root_tetris, 1370 , 750);
+        
+        Tetris_Stage.setTitle("Tetris");
+        Tetris_Stage.setResizable(false);
+        Tetris_Stage.setMaximized(true);
+        Tetris_Stage.setScene(scene);
+        Tetris_Stage.show();
+    }
+    
+}
