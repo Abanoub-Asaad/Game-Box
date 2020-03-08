1461109cb66c73fc9c7bbf5c8af3bcbdd2e0ab0d
nada-elsayed-anies <49215297+nada-elsayed-anies@users.noreply.github.com>
2020-03-08 05:01:52 +0200

    Update map.java

--- creatmap/src/creatmap/map.java
+++ creatmap/src/creatmap/map.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package creatmap;
 
-import java.awt.Graphics;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
